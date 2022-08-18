1b6e4a357832328569f3a8bd09fa9cd253fa63b5
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-18 15:17:40 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,9 @@
+#！警告：replit免费版中所有代码都是公开可见的，建议使用edu版部署
+# !RUN之前请按照 create-db.md 的指示创建数据库
+
+sh startpg.sh
+pg_ctl stop
+pg_ctl -l ./postgresql.log start
 
 if [ ! -f "gitea" ];then
 curl -L https://github.com/go-gitea/gitea/releases/download/v1.17.0/gitea-1.17.0-linux-amd64 -o gitea
